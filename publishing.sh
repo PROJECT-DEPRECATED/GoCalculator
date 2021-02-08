c61c1ad8aa7737137beed6c5fcdc1fd5733e6d26
Project_TL <kimpiao020@gmail.com>
2021-02-08 19:32:21 +0900

    Fix: Fix bug

--- publishing.sh
+++ publishing.sh
@@ -9,8 +9,8 @@ go build src/Calculator.go
 mkdir build/
 mv Calculator build/
 
-mv Calculator Calculator-$version
-cp Calculator-$version Calculator-$version.exe
+mv build/Calculator Calculator-$version
+cp build/Calculator-$version Calculator-$version.exe
 
 cp RunScript/Calculator.sh build/
 zip Calculator-1.0v.zip build/
